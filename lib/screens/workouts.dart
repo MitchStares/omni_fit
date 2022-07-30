66330ff4fe0fb3c8edcc6e019509eeaea6b68d5b
Mitch Stares <mitchell.stares@hotmail.com>
2022-07-30 10:00:22 +1000

    Added colour selection on tap

--- lib/screens/workouts.dart
+++ lib/screens/workouts.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
-class WorkoutPage extends StatelessWidget {
+class WorkoutPage extends StatefulWidget {
+  const WorkoutPage({super.key});
+  @override
+  State<WorkoutPage> createState() => _WorkoutPageState();
+}
+
+class _WorkoutPageState extends State<WorkoutPage> {
+  bool _selected = false;
+  int selectedIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+  }
+
   static const List<Exercise> entries = [
     Exercise(0, 'Machine Leg Press', 12),
     Exercise(1, 'Lying Leg Curl', 15),
@@ -9,9 +22,9 @@ class WorkoutPage extends StatelessWidget {
   final List<String> exercises = entries.map((set) => set.name).toList();
   final List<int> reps = entries.map((number) => number.reps).toList();
   final List<int> colorCodes = <int>[600, 500, 100];
-
-  WorkoutPage({super.key});
-
+  // todo: this needs to be changed, has a hard coded value of 200
+  // final List<bool> _selected =
+  //     List.generate(200, (i) => false); // Pre filled list
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
@@ -19,12 +32,46 @@ class WorkoutPage extends StatelessWidget {
       itemCount: entries.length,
       itemBuilder: (BuildContext context, int index) {
         return Container(
-          height: 50,
-          // color: Colors.amber[colorCodes[index]],
-          color: Colors.grey[colorCodes[index]],
-          child: Row(
-              children: [Text(exercises[index]), Text('Reps: ${reps[index]}')]),
-        );
+            height: 50,
+            // color: Colors.amber[colorCodes[index]],
+            // color: _selected == true
+            //     ? selectedIndex == index
+            //         ? Colors.amber
+            //         : Colors.transparent
+            //     : Colors.transparent,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ListTile(
+                    leading: GestureDetector(
+                      behavior: HitTestBehavior.translucent,
+                      onTap: () {
+                        setState(() {
+                          _selected = !_selected;
+                          selectedIndex = index;
+                        });
+                      },
+                      child: Container(
+                        width: 48,
+                        height: 48,
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        alignment: Alignment.center,
+                        child: CircleAvatar(
+                          backgroundColor: _selected == true
+                              ? selectedIndex == index
+                                  ? Colors.amber
+                                  : Colors.lightBlue
+                              : Colors.lightBlue,
+                        ),
+                      ),
+                    ),
+                    title: Text(exercises[index]),
+                    trailing: Text('Reps: ${reps[index]}'),
+                    dense: true,
+                  ),
+                ),
+              ],
+            ));
       },
       separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
